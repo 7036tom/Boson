c13e5799889e127d8d0fb1485f039b89555c0a20
7036tom <tom.dagens@centrale-marseille.fr>
2017-03-28 18:04:35 +0200

    last commit

--- Best_model.py
+++ Best_model.py
@@ -1,6 +1,6 @@
 # Create first network with Keras
 from keras.models import Sequential
-from keras.layers import Dense, Reshape, Activation, Dropout,Layer, LocallyConnected1D, LocallyConnected2D, Convolution1D, GlobalMaxPooling1D, Flatten, MaxPooling1D, MaxPooling2D
+from keras.layers import Dense, Reshape, Activation, Dropout,Layer, LocallyConnected1D, LocallyConnected2D, Convolution1D, GlobalMaxPooling1D, Flatten, MaxPooling1D, MaxPooling2D, Merge
 from keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau, LearningRateScheduler
 from keras.optimizers import SGD
 from keras.optimizers import RMSprop, Adamax
@@ -77,20 +77,23 @@ dataset = dataframe.values
 dataframe2 = pandas.read_csv("test.csv", header=None)
 dataset2 = dataframe2.values
 
-# Let's move all the azymuth angle features at the end of X (12, 16, 19, 21, 26, 29)
+# Parametres
+Seuil = 0.5
+
+# Let's move all the azymuth angle features at the end of X (12, 16, 19, 21, 16, 29)
 # 12 <-> 28 PhiCentrality
 dataset[:,[12,28]]
 #dataset2[:,[12,28]]
-# 16 <-> 27 Phitau
-dataset[:,[16,26]]
-#dataset2[:,[16,26]]
-# 19 <-> 25 Philep
-dataset[:,[19,25]]
-#dataset2[:,[19,25]]
-# 21 <-> 24 Phimet
-dataset[:,[21,24]]
-#dataset2[:,[21,24]]
-# 26 Phijetleading
+# 16 <-> 16 Phitau
+dataset[:,[16,16]]
+#dataset2[:,[16,16]]
+# 19 <-> 19 Philep
+dataset[:,[19,19]]
+#dataset2[:,[19,19]]
+# 21 <-> 21 Phimet
+dataset[:,[21,21]]
+#dataset2[:,[21,21]]
+# 16 Phijetleading
 # 29 Phijetsubleading
 
 
@@ -98,57 +101,105 @@ dataset[:,[21,24]]
 W = dataset[0:250000:,31:32]
 X = dataset[0:10000:,1:31].astype(float)
 Y = dataset[0:10000:,32].astype(float)
-X2 = dataset[0:250000:,1:24].astype(float)
-Y2 = dataset[0:250000:,32].astype(float)
+X2 = dataset[0:250000:,1:31].astype(float)
 
-Z2 = dataset2[0:550000,1:24].astype(float)
+
+Y2 = dataset[0:250000:,32].astype(float)
+Z2 = dataset2[0:550000,1:31].astype(float)
+
+
 
 # Implementation of advanced features in x and x2
 # Notes : minv(tau,lep) (3) | 
 
-"""
-for i in range(9999):
-	# Implementation of Phi derived features
-	X[i,24] = min(dataset[i,26]-dataset[i,25],dataset[i,26]-dataset[i,24],dataset[i,25]-dataset[i,24])
-	X[i,25] = min(dataset[i,26]-dataset[i,24],dataset[i,25]-dataset[i,24])
-	X[i,26] = min(dataset[i,26]-dataset[i,25],dataset[i,26]-dataset[i,24])
-	X[i,27] = dataset[i,25]-dataset[i,24] # Erreur ?
-	# Implementation of mass based features
-	#print(dataset[1, 3])
-	#x[i,4] = log(1+dataset[i, 3])
-	
 
-
-for i in range(220000):
+for i in range(250000):
 	# Implementation of Phi derived features
-	X2[i,24] = min(dataset[i,26]-dataset[i,25],dataset[i,26]-dataset[i,24],dataset[i,25]-dataset[i,24])
-	X2[i,25] = min(dataset[i,26]-dataset[i,24],dataset[i,25]-dataset[i,24])
-	X2[i,26] = min(dataset[i,26]-dataset[i,25],dataset[i,26]-dataset[i,24])
-	X2[i,27] = dataset[i,25]-dataset[i,24] # Erreur ?
+	X2[i,20] = min(dataset[i,16]-dataset[i,19],dataset[i,16]-dataset[i,21],dataset[i,19]-dataset[i,21])
+	X2[i,18] = min(dataset[i,16]-dataset[i,21],dataset[i,19]-dataset[i,21])
+	X2[i,15] = min(dataset[i,16]-dataset[i,19],dataset[i,16]-dataset[i,21])
+	X2[i,27] = dataset[i,19]-dataset[i,21]
 
 	# Implementation of mass based features
 	#x2[i,4] = log(1)#+dataset[i, 3])
 
 for i in range(550000):
 	# Implementation of Phi derived features
-	Z2[i,24] = min(dataset2[i,26]-dataset2[i,25],dataset2[i,26]-dataset2[i,24],dataset2[i,25]-dataset2[i,24])
-	Z2[i,25] = min(dataset2[i,26]-dataset2[i,24],dataset2[i,25]-dataset2[i,24])
-	Z2[i,26] = min(dataset2[i,26]-dataset2[i,25],dataset2[i,26]-dataset2[i,24])
-	Z2[i,27] = dataset2[i,25]-dataset2[i,24] # Erreur ?
-
+	Z2[i,20] = min(dataset2[i,16]-dataset2[i,19],dataset2[i,16]-dataset2[i,21],dataset2[i,19]-dataset2[i,21])
+	Z2[i,18] = min(dataset2[i,16]-dataset2[i,21],dataset2[i,19]-dataset2[i,21])
+	Z2[i,15] = min(dataset2[i,16]-dataset2[i,19],dataset2[i,16]-dataset2[i,21])
+	Z2[i,27] = dataset2[i,19]-dataset2[i,21]
+"""
 	# Implementation of mass based features
 	#x2[i,4] = log(1)#+dataset[i, 3])
 """
+# Replacing -999 by average of non-999
+
+Missing = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+nb_missing = 0
+for i in range(250000):
+	for j in range(30):
+		if (X2[i][j]==-999.0):
+			Missing[j]=1
+
+Missing_position = [0,0,0,0,0,0,0,0,0,0,0]
+p = 0
+for i in range(30):
+	if (Missing[i]==1):
+		Missing_position[p]=i
+		p = p + 1
 
 
-X -= np.mean(X, axis = 0) # center
+Mean = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+
+q = 0
+for i in Missing_position:
+	p = 0
+	for j in range(250000):
+		if (X2[j][i]!=-999.0):
+			Mean[q]+=X2[j][i]
+			p += 1			
+	Mean[q] /= p
+	q += 1
+
+q = 0
+for i in Missing_position:
+	for j in range(250000):
+		if (X2[j][i]==-999.0):
+			X2[j][i] = Mean[q]
+	q +=1
+	
+Mean = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+
+q = 0
+for i in Missing_position:
+	p = 0
+	for j in range(550000):
+		if (Z2[j][i]!=-999.0):
+			Mean[q]+=Z2[j][i]
+			p += 1		
+	Mean[q] /= p
+	q += 1
+
+q = 0
+for i in Missing_position:
+	for j in range(550000):
+		if (Z2[j][i]==-999.0):
+			Z2[j][i] = Mean[q]
+	q += 1			
+
+
+
+
+
+
 X2 -= np.mean(X2, axis = 0) # center
 Z2 -= np.mean(Z2, axis = 0) # center
 
 
 #Normalisation of inputs.
 
-X /= np.std(X, axis = 0) # normalize
+
 X2 /= np.std(X2, axis = 0) # normalize
 Z2 /= np.std(Z2, axis = 0) # normalize
 
@@ -166,7 +217,6 @@ Y = np_utils.to_categorical(Y, 2) # convert class vectors to binary class matric
 
 #input_shape=(28,1)
 model = [Sequential(), Sequential(),Sequential(), Sequential(), Sequential()]
-
 #rms = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0001)
 
 
@@ -202,17 +252,16 @@ for i in range(1):
 	for train, test in kfold.split(X2, Y2):
 		print(j) 
 		print("ieme fold")
-		if (j == 5):
-			j = 0
+		
 		# create model(135/75/105)
-		model[j].add(Dense(285, input_dim=23, init='normal', activation='relu' ,W_regularizer=l1l2(l1=9E-7, l2=5e-07))) #W_regularizer=l1(0.000001), activity_regularizer=activity_l1(0.000001)))
-		model[j].add(Dropout(0.4))
+		model[j].add(Dense(285, input_dim=30, init='normal', activation='relu' ,W_regularizer=l1l2(l1=9E-7, l2=5e-07))) #W_regularizer=l1(0.000001), activity_regularizer=activity_l1(0.000001)))
+		model[j].add(Dropout(0.35))
 		model[j].add(L)
 		model[j].add(Dense(360,  activation ='relu'))
-		model[j].add(Dropout(0.4))
+		model[j].add(Dropout(0.35))
 		model[j].add(L)
 		model[j].add(Dense(270,  activation ='relu'))
-		model[j].add(Dropout(0.4))
+		model[j].add(Dropout(0.35))
 		model[j].add(L)
 
 		model[j].add(Dense(2))
@@ -247,9 +296,9 @@ for i in range(1):
 		
 		
 		# Fit the model		
-		history = model[j].fit(X2[train], Y3[train],validation_data=(X2[test], Y3[test]), nb_epoch=200, batch_size=93, class_weight=class_weight, shuffle=True, verbose=1, callbacks=callbacks)#, class_weight=class_weight)
+		model[j].fit(X2[train], Y3[train],validation_data=(X2[test], Y3[test]), nb_epoch=100, batch_size=93, class_weight=class_weight, shuffle=True, verbose=1, callbacks=callbacks)#, class_weight=class_weight)
 
-		
+		"""
 		# list all data in history
 		print(history.history.keys())
 		# summarize history for accuracy
@@ -269,7 +318,7 @@ for i in range(1):
 		plt.legend(['train', 'test'], loc='upper left')
 		plt.show()
 		#model.fit(X2[train], Y3[train], nb_epoch=200, batch_size=96)
-
+		"""
 		
 		# evaluate the model
 		Z_eval = model[j].predict(X2[test], batch_size=32, verbose=0)
@@ -298,6 +347,10 @@ for i in range(1):
 print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
 
 
+merged_model = Sequential()
+merged_model.add(Merge(model, mode='ave'))
+
+
 """
 model.fit(X2, Y2, validation_data=(X, Y), nb_epoch=200, batch_size=96)
 # Final evaluation of the model
@@ -315,17 +368,13 @@ print("Baseline Error: %.2f%%" % (100-scores[1]*100))
 
 # AMS on train file.
 
-Z0_train_restult = model[0].predict(X2, batch_size=32, verbose=0)
-Z1_train_restult = model[1].predict(X2, batch_size=32, verbose=0)
-Z2_train_restult = model[2].predict(X2, batch_size=32, verbose=0)
-Z3_train_restult = model[3].predict(X2, batch_size=32, verbose=0)
-Z4_train_restult = model[4].predict(X2, batch_size=32, verbose=0)
+Result_local = merged_model.predict([X2, X2, X2, X2, X2], batch_size=32, verbose=0)
 
 s = 0
 b = 0
 
 for i in range(250000):
-	if (Z0_train_restult[i][1]+Z1_train_restult[i][1]+Z2_train_restult[i][1]+Z3_train_restult[i][1]+Z4_train_restult[i][1]> Z0_train_restult[i][0]+Z1_train_restult[i][0]+Z2_train_restult[i][0]+Z3_train_restult[i][0]+Z4_train_restult[i][0]):
+	if (Result_local[i][1] > Result_local[i][0] and Result_local[i][1] > Seuil):
 		if (Y2[i]==1):
 			s = s + W[i]
 		if (Y2[i]==0):
@@ -342,19 +391,16 @@ print(AMS)
 # AMS on test file.
 
 
-Z0_restult = model[0].predict(Z2, batch_size=32, verbose=0)
-Z1_restult = model[1].predict(Z2, batch_size=32, verbose=0)
-Z2_restult = model[2].predict(Z2, batch_size=32, verbose=0)
-Z3_restult = model[3].predict(Z2, batch_size=32, verbose=0)
-Z4_restult = model[4].predict(Z2, batch_size=32, verbose=0)
+Result_test = merged_model.predict([Z2, Z2, Z2, Z2, Z2], batch_size=32, verbose=0)
+
 
 
 c = csv.writer(open("Submission.csv", "wb"))
 c.writerow(["EventId","RankOrder","Class"])
 for i in range(550000):
-	if (Z0_restult[i][1]+Z1_restult[i][1]+Z2_restult[i][1]+Z3_restult[i][1]+Z4_restult[i][1]> Z0_restult[i][0]+Z1_restult[i][0]+Z2_restult[i][0]+Z3_restult[i][0]+Z4_restult[i][0]):
+	if (Result_test[i][1] > Result_test[i][0] and Result_test[i][1] > Seuil):
 		c.writerow([350000+i,i+1,'s' ])
-	if (Z0_restult[i][1]+Z1_restult[i][1]+Z2_restult[i][1]+Z3_restult[i][1]+Z4_restult[i][1]<= Z0_restult[i][0]+Z1_restult[i][0]+Z2_restult[i][0]+Z3_restult[i][0]+Z4_restult[i][0]):
+	else:
 		c.writerow([i+350000,i+1,'b' ])
 
 
